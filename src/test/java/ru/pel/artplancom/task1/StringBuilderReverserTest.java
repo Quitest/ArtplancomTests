bdac4bd25ec101660bc2a4b5daf02e16ffa8448a
lenovo <none@none.org>
2022-07-08 14:28:56 +0300

    Написаны простейшие тесты.

--- src/test/java/ru/pel/artplancom/task1/StringBuilderReverserTest.java
+++ src/test/java/ru/pel/artplancom/task1/StringBuilderReverserTest.java
@@ -1,10 +1,20 @@
 package ru.pel.artplancom.task1;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class StringBuilderReverserTest {
 
-    @org.junit.jupiter.api.Test
-    void reverse() {
+    @Test
+    void reverse_expectedCorrect() {
+        String str = "reversed string";
+        assertEquals("gnirts desrever", new StringBuilderReverser().reverse(str));
+    }
+
+    @Test
+    void reverseNull_expectedNull() {
+        assertEquals("", new StringBuilderReverser().reverse(null));
     }
 }
